6e7c2dd452f4d914147507d63ef4f25dcbff48d6
shaowen <shaowen310@outlook.com>
2024-11-20 18:31:15 +0800

    update scripts

--- Long-term_Forecasting/run.sh
+++ Long-term_Forecasting/run.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=6
+export CUDA_VISIBLE_DEVICES=7
 
 bash scripts/ETTh1.sh
 bash scripts/ETTh2.sh
@@ -6,3 +6,8 @@ bash scripts/ETTm1.sh
 bash scripts/ETTm2.sh
 bash scripts/weather.sh
 bash scripts/exchange.sh
+
+# bash scripts/ETTh1_to_ETTh2.sh
+# bash scripts/ETTh2_to_ETTh1.sh
+# bash scripts/ETTm1_to_ETTm2.sh
+# bash scripts/ETTm2_to_ETTm1.sh
